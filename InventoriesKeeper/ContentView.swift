886b322d887d162b73a3f76529eb9513dcaca7d2
berlinsk <berliniumyar@gmail.com>
2025-07-01 01:25:06 +0300

    Initial Commit

---
+++ InventoriesKeeper/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  InventoriesKeeper
+//
+//  Created by Берлинский Ярослав Владленович on 30.06.2025.
+//
+
+import SwiftUI
+import RealmSwift
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InventoriesKeeper/InventoriesKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoriesKeeperApp.swift
+//  InventoriesKeeper
+//
+//  Created by Берлинский Ярослав Владленович on 30.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct InventoriesKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
